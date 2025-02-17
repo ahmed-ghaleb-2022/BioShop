bc7e69d832f73b00bd0f28d697bf5ff32786b862
ahmed-ghaleb-2022 <ahmedtareq19932022@gmail.com>
2025-02-17 16:36:11 +0100

    upload the project with some components

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideFirebaseApp(()=> initializeApp(environment.firebase))]
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  featureFlag: true,
+  firebase: {
+    apiKey: 'AIzaSyDbvyXLS7foiz7izh2-AMxRHtghhcZs6wk',
+    authDomain: 'bioshop-680b2.firebaseapp.com',
+    projectId: 'bioshop-680b2',
+    storageBucket: 'bioshop-680b2.firebasestorage.app',
+    messagingSenderId: '1024802171233',
+    appId: '1:1024802171233:web:901df53595d491ef29fd20',
+  },
+};
